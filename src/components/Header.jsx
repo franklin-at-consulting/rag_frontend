595ce8b6cdb53f4d99967fce683562206ac90c00
Nestor Perez <njperez1994@gmail.com>
2025-02-07 13:19:43 -0500

    fix issues

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -53,7 +53,7 @@ export function Header() {
   };
 
   return (
-    <header className="flex items-center justify-between pr-4 pb-3 pt-3">
+    <header className="flex justify-between pr-4 pb-3 pt-3">
       {/* Logo and App Name */}
       <div className="flex items-center space-x-2">
         <img src={logo} alt="App Logo" className="w-8 h-8" />
