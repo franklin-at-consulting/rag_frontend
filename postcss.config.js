d1673d13f205b7486312c553438f78efb8d9c0dc
Nestor Perez <njperez1994@gmail.com>
2025-02-07 08:13:25 -0500

    solving issues

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 }
-=======
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
->>>>>>> 0c793ad (4.0 version)
+
